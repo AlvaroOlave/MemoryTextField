50ddada05892ed52cd33e2f8c64b496e37b11967
Alvaro <alvaro.olave@grupocmc.es>
2016-10-31 13:15:49 +0100

    Initial commit

---
+++ AOMemoryTextField/AOMemoryTextField.h
@@ -0,0 +1,19 @@
+//
+//  AOMemoryTextField.h
+//  AOMemoryTextField
+//
+//  Created by Alvaro on 30/10/16.
+//  Copyright © 2016 Alvaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOMemoryTextField.
+FOUNDATION_EXPORT double AOMemoryTextFieldVersionNumber;
+
+//! Project version string for AOMemoryTextField.
+FOUNDATION_EXPORT const unsigned char AOMemoryTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOMemoryTextField/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+# Uncomment this line to define a global platform for your project
+platform :ios, ‘8.0’
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
+
+target "AOMemoryTextField" do
+    pod 'MemoryTextField', :path => '~/Documents/AO/AOMemoryTextField'
+end
